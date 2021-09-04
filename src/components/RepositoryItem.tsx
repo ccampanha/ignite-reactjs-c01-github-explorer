299b03778bed4a7faef968ca338af4e34ea6ae26
ccampanha <cristiana.campanha@gmail.com>
2021-09-04 16:17:18 -0300

    github explorer

---
+++ src/components/RepositoryItem.tsx
@@ -0,0 +1,19 @@
+interface  RepositoryItemProps {
+  repository: {
+    name: String;
+    description: string;
+    html_url: string;
+  }
+
+}
+
+
+export function RepositoryItem(props: RepositoryItemProps) {
+  return(
+    <li>
+      <strong>{props.repository.name}</strong>
+      <p>{props.repository.description}</p>
+      <a href={props.repository.html_url} target="_blank">Acessar Repositório</a>
+    </li>
+  )
+}
